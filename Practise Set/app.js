6c66bed1fc1a10f7f993fa8c4737ceed59ee8f62
Subin Kumar <sadiesink718@gmail.com>
2025-06-15 15:14:07 +0530

    practise-set

--- Practise Set/app.js
+++ Practise Set/app.js
@@ -35,7 +35,6 @@ const server = http.createServer((req, res) => {
         return res.end();
     }
 });
-
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
