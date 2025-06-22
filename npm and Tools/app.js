c5e83f76cca3de60844fa093e3a37d101f2b5dde
Subin Kumar <sadiesink718@gmail.com>
2025-06-23 00:49:31 +0530

    npm-tools

--- npm and Tools/app.js
+++ npm and Tools/app.js
@@ -1,12 +1,11 @@
 const http = require('http');
 
-
-const server = http.createServer((req,res) => {
-    console.log(req);
-    
+const server = http.createServer((req, res) => {
+    console.log(`Request URL: ${req.url}, Method: ${req.method}`);
+    res.end('Hello from server'); // Send a response to prevent hanging
 });
 
-const PORT = 3000;
+const PORT = 3001;
 server.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
